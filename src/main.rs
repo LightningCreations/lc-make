72532a9c0b33531aeece2d7bec4f99b9f14d143d
Connor Horman <chorman64@gmail.com>
2021-06-16 10:50:53 -0400

    Switch to argparse (#4)
    * Start switch to argparse
    
    * Fix nitpicks

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,58 @@
-use clap::{App, Arg};
+use argparse::ArgumentParser;
 
-use std::env;
 use std::fs::File;
-use std::path::Path;
+use std::path::PathBuf;
 
 use lc_make::loader::MakeFileLoader;
 
 fn main() -> std::io::Result<()> {
-    let matches = App::new("LC Make")
-        .version("0.1.0")
-        .author("Ray Redondo <rdrpenguin04@gmail.com>")
-        .arg(
-            Arg::with_name("dir")
-                .short("C")
-                .long("directory")
-                .takes_value(true)
-                .help("Change to <dir> before doing anything"),
-        )
-        .arg(
-            Arg::with_name("file")
-                .short("f")
-                .takes_value(true)
-                .help("Use <file> as a makefile"),
-        )
-        .arg(
-            Arg::with_name("silent")
-                .short("s")
-                .help("Don't echo recipes"),
-        )
-        .arg(Arg::with_name("target"))
-        .get_matches();
-
-    // handle -C flag
-    if let Some(dir) = matches.value_of("dir") {
-        env::set_current_dir(Path::new(dir))?;
+    let mut dir = None::<PathBuf>;
+    let mut file = None::<PathBuf>;
+    let mut silent = false;
+    let mut target = None::<String>;
+    let mut ap = ArgumentParser::new();
+    ap.refer(&mut dir).add_option(
+        &["-C"],
+        argparse::StoreOption,
+        "Change to the given directory before doing anything else",
+    );
+    ap.refer(&mut file).add_option(
+        &["-f"],
+        argparse::StoreOption,
+        "Use <file> as the Makefile instead of Makefile",
+    );
+    ap.refer(&mut target)
+        .add_argument("TARGET", argparse::ParseOption, "TARGET to build");
+    ap.refer(&mut silent).add_option(
+        &["--silent", "-s", "--quiet"],
+        argparse::StoreTrue,
+        "Prevents make from outputting anything",
+    );
+    ap.parse_args_or_exit();
+    drop(ap);
+    if let Some(dir) = dir {
+        std::env::set_current_dir(dir)?;
     }
-
-    // if the user specified a makefile then use that file
-    // otherwise try and find one from a list of defaults
-    let file = if let Some(file) = matches.value_of("file") {
-        Some(File::open(file))
+    let file = if let Some(file) = file {
+        File::open(file)
     } else {
         let defaults = vec!["GNUmakefile", "makefile", "Makefile"];
 
-        defaults.into_iter().map(File::open).find(Result::is_ok)
+        defaults
+            .into_iter()
+            .map(File::open)
+            .find(Result::is_ok)
+            .ok_or_else(|| {
+                std::io::Error::new(std::io::ErrorKind::NotFound, "Cannot find makefile")
+            })
+            .and_then(|s| s)
     };
 
     // create a new makefile loader
     let mut loader = MakeFileLoader::new();
 
     // if we have a valid file then load the makefile's contents
-    if let Some(Ok(mut file)) = file {
+    if let Ok(mut file) = file {
         loader.load(&mut file)?;
     }
 
@@ -58,8 +60,7 @@ fn main() -> std::io::Result<()> {
     let makefile = loader.finalise();
 
     // perform the build
-    let silent = matches.is_present("silent");
-    if let Some(target) = matches.value_of("target") {
+    if let Some(target) = target {
         // don't be silent for debugging purposes
         makefile.build_target(target, silent);
     } else {
